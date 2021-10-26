b60c8ffbc1fe6d37d0f1cd62592ec7122ec79af1
lazari <lazari.dev@gmail.com>
2021-10-26 20:48:00 +0300

    Impl react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Switch} from "react-router-dom";
+import {Routes} from "./Routes";
 
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
+            <Switch><Routes/></Switch>
+        </BrowserRouter>
     </div>
   );
 }
