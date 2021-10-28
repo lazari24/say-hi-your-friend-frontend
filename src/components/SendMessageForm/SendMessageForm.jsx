418f930429f279f8f0c64db597d1fabed9543a82
lazari <lazari.dev@gmail.com>
2021-10-28 12:38:54 +0300

    add validation rule

--- src/components/SendMessageForm/SendMessageForm.jsx
+++ src/components/SendMessageForm/SendMessageForm.jsx
@@ -9,9 +9,6 @@ const layout = {
 /* eslint-disable no-template-curly-in-string */
 const validateMessages = {
   required: '${label} is required!',
-  types: {
-    email: '${label} is not a valid email!',
-  },
 };
 
 export const SendMessageForm = () => {
@@ -22,18 +19,18 @@ export const SendMessageForm = () => {
   return (
     <div className="form-wrapper">
       <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} style={{width: "400px"}}>
-        <Form.Item name={['user', 'name']} rules={[{ required: true }]}>
+        <Form.Item name={['name']} rules={[{ required: true }]} hasFeedback>
           <Input placeholder="I'm..."/>
         </Form.Item>
-        <Form.Item name={['email']} rules={[{ type: 'email', required: true }]}>
-          <Input placeholder="Email"/>
+        <Form.Item name={['friendName']} hasFeedback rules={[{required: true }]}>
+          <Input placeholder="Your friend name..."/>
         </Form.Item>
-        <Form.Item name={['user', 'introduction']} rules={[{required: true}]}>
+        <Form.Item name={['message']} hasFeedback rules={[{required: true}]}>
           <Input.TextArea placeholder="Your message"/>
         </Form.Item>
         <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 0 }}>
           <Button type="default" htmlType="submit">
-            Отправить
+            Send
           </Button>
         </Form.Item>
       </Form>
