f4b2c4c26330d75299531f59d1f5b7e3297f163f
lazari <lazari.dev@gmail.com>
2021-10-28 11:22:27 +0300

    fix header's links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,31 @@
 import './Header.css'
 import {MessageOutlined, GithubOutlined, LinkedinOutlined} from "@ant-design/icons";
+import {Link} from 'react-router-dom';
 
 export const Header = () => {
   return (
     <header className="app-header">
       <div className="header-root">
         <div className="left-header-block">
-          <MessageOutlined style={{
-            fontSize: '35px',
-            paddingRight: '15px',
-          }}/>
-          <span className="logo-text">SAY HI YOUR FRIEND!</span>
+          <Link to="/">
+              <MessageOutlined style={{
+                fontSize: '35px',
+                paddingRight: '15px',
+              }}/>
+              <span className="logo-text">SAY HI YOUR FRIEND!</span>
+          </Link>
         </div>
         <div className="right-header-block">
-          <div className="icon-container-right"><GithubOutlined/></div>
-          <div className="icon-container-right"><LinkedinOutlined /></div>
+          <div className="icon-container-right">
+            <a href="https://github.com/lazari24" target="_blank" rel="noreferrer">
+              <GithubOutlined/>
+            </a>
+          </div>
+          <div className="icon-container-right">
+            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+              <LinkedinOutlined />
+            </a>
+          </div>
         </div>
       </div>
     </header>
---
+++ src/components/UrlBox/UrlBox.tsx
@@ -0,0 +1,7 @@
+export const UrlBox = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
